76cb04c1cf0e4de6157e2fd8d8057ae9d862bac0
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-03-17 10:30:17 +0530

    2022/3/17
    created the table using labels and added border to the created table.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+from operator import index
 from tkinter import *
 import random
-from unittest import result
 
+#()=rows
+tableDigitsList = [(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"",""),(1,"",23,33,"",31,"","","",11,34,"",12,67,78,"",90,98,"","")]
 
 #constances
 GAME_WIDTH = 500
 GAME_HEIGHT = 500
-SPACE_WIDTH = 25
+SPACE_WIDTH = 50
 BACKGROUND_COLOR = 'black'
 LINE_COLOR = TEXT_COLOR = 'white'
 CURRENT_GUESSBOX_LINE_COLOR = 'blue'
@@ -33,12 +35,24 @@ window.resizable(False, False)
 
 score = 0
 result = random.randint(0,1000)
+no_columns = int(GAME_WIDTH/SPACE_WIDTH)
+no_rows = int(GAME_HEIGHT/SPACE_WIDTH)
 
 math_operation_label = Label(window,text="{} = {}".format(randomResultDigitsGenerator(),result),font=('consalas',20),bg=BACKGROUND_COLOR,fg = TEXT_COLOR)
 math_operation_label.pack()
 
-canvas = Canvas(window,bg = BACKGROUND_COLOR, width = GAME_WIDTH, height = GAME_HEIGHT)
-canvas.pack()
+
+frame = Frame(window,bg = BACKGROUND_COLOR, width = GAME_WIDTH, height = GAME_HEIGHT)
+frame.pack()
+
+#create the list for showing tableDigits
+for i in range(no_rows): #Rows
+    for j in range(no_columns): #Columns
+        label = Label(frame, text="123",borderwidth=2,relief="groove",bg=BACKGROUND_COLOR,fg="white",width=int(SPACE_WIDTH/10),height=int(SPACE_WIDTH/20))
+        # label.place(relx=.5, rely=.5)
+        label.grid(row=i,column=j, sticky=NSEW)
+        
+
 
 score_label = Label(window, text='score:{}'.format(score),font=('consalas',40))
 score_label.pack()
